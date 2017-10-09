a8e2ef20365cfd8bbfaa8e095566f3cf1bac5f17
Artem <stratienko.artem@gmail.com>
2017-10-09 11:41:52 +0300

    Add 'Alamofire' pods.

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '11.0'
+use_frameworks!
+
+target 'Application_ATeam' do
+    pod 'Alamofire'
+end
---
+++ Pods/Target Support Files/Pods-Application_ATeam/Pods-Application_ATeam-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Application_ATeam : NSObject
+@end
+@implementation PodsDummy_Pods_Application_ATeam
+@end
